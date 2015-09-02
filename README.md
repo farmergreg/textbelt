dc32122f03956c8551ede4641b8af1260b8b3a4a
Greg Dietsche <Gregory.Dietsche@cuw.edu>
2015-09-01 19:29:38 -0500

    initial commit
    Signed-off-by: Greg Dietsche <Gregory.Dietsche@cuw.edu>

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# textbelt
-golang library for textbelt.com
+textbelt.com API in GO  [![GoDoc](https://godoc.org/github.com/dietsche/textbelt?status.png)](https://godoc.org/github.com/dietsche/textbelt)
+==================
+
+This library sends text messages using http://textbelt.com
+
+## Installing
+
+### go get
+    $ go get github.com/dietsche/textbelt
+
+### Example Code
+    import (
+        "github.com/dietsche/textbelt"
+    )
+    
+    func main() {
+        texter := textbelt.New()
+        yourPhone := "123-456-7890"
+        if err := texter.Text(yourPhone, "txt msg from go!"); err != nil {
+            painc(err)
+        }
+    }
---
+++ textbelt.go
@@ -0,0 +1,71 @@
+package textbelt
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+const (
+	// Send text messages to the USA
+	TextbeltAPIusa = "http://textbelt.com/text"
+
+	// Send text messages to Canada
+	TextbeltAPIcanada = "http://textbelt.com/canada"
+
+	// Send text messages internationally
+	TextbeltAPIinternational = "http://textbelt.com/intl"
+)
+
+type textbeltResponse struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+}
+
+// Client is your entry point into the rest of the library.
+type Client struct {
+	apiURL string
+}
+
+// New creates a new client that will use http://textbelt.com to send text messages in the USA.
+func New() *Client {
+	c := &Client{}
+	c.apiURL = TextbeltAPIusa
+	return NewClientFromURL(TextbeltAPIusa)
+}
+
+// NewClientFromURL creates a new client using a url of your choosing.
+func NewClientFromURL(apiURL string) *Client {
+	c := &Client{}
+	c.apiURL = apiURL
+	return c
+}
+
+// Text sends a text message.
+func (c *Client) Text(phoneNumber string, message string) error {
+	data := url.Values{
+		"number":  {phoneNumber},
+		"message": {message},
+	}
+
+	response, err := http.PostForm(c.apiURL, data)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	responseText, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+
+	i := textbeltResponse{}
+	err = json.Unmarshal([]byte(responseText), &i)
+
+	if !i.Success {
+		return errors.New(i.Message)
+	}
+	return nil
+}
